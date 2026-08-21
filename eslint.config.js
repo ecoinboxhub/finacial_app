39b2bc00ca81e913982f990d21893fd628b15dce
EngrIBGIT <ibrahim5322022@hotmail.com>
2026-08-21 11:52:16 +0100

    fix: Add vite/client types to tsconfig and broaden eslint ignores to fix CI typecheck

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'node_modules', 'android', 'server', '.pytest_cache', '.vercel', 'public', '*.apk']),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
